81c74a96055de21e52e8d0efbdb727755912153e
kyeonghooon <gibbon0510@gmail.com>
2025-09-13 13:21:57 +0900

    first commit

---
+++ src/main/java/com/qmate/qmate/QMateApplication.java
@@ -0,0 +1,13 @@
+package com.qmate.qmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QMateApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(QMateApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/qmate/qmate/QMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qmate.qmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QMateApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
