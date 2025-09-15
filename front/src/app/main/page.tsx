f93b6daf26ae4bdf8e753ba2cd3f60de2fc9fb26
leejicircle <leeji.circle@gmail.com>
2025-09-16 00:38:46 +0900

    design:main  ui 배치

--- front/src/app/main/page.tsx
+++ front/src/app/main/page.tsx
@@ -1,10 +1,36 @@
-import { ExpBar } from "@/components/main/ExpBar";
-import React from "react";
+import Bubbley from '@/components/main/Bubbley';
+import { ExpBar } from '@/components/main/ExpBar';
+import ExpBubble from '@/components/main/ExpBubble';
+import Image from 'next/image';
+import React from 'react';
 
 const page = () => {
   return (
-    <div className="w-full h-screen bg-gradient-main">
-      <ExpBar />
+    <div className="w-full min-h-screen flex flex-col items-center justify-center bg-gradient-main">
+      <div className="pointer-events-none z-0">
+        <Image
+          src="/images/light.png"
+          alt="빛 효과 이미지"
+          fill
+          className="object-contain object-top"
+          priority
+        />
+        <Image
+          src="/images/deco.png"
+          alt="배경 장식 이미지"
+          priority
+          fill
+          sizes="100vw"
+          className="object-fill object-bottom"
+        />
+      </div>
+
+      <div className="relative z-1 flex flex-col items-center justify-center w-[252px] h-[358px]">
+        <ExpBubble />
+        {/** TODO: API 연동 후 경험치 값 props로 받기 */}
+        <Bubbley exp={0} className="mb-6" />
+        <ExpBar />
+      </div>
     </div>
   );
 };
