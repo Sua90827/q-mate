0fab845ec4887961e418005f0ef810a58e5f8d90
유현지 <bwj9808@gmail.com>
2025-09-15 21:32:33 +0900

    feat 커스텀 질문 등록하기

---
+++ front/src/components/question/Custom.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from '../common/Button';
+
+export default function Custom() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gradient-sub">
+      <div className="flex flex-col h-[246px]">
+        <span className="font-bold text-[24px]  pb-5">궁금한 질문 작성하기</span>
+        <textarea
+          placeholder="내용을 입력해주세요"
+          className="md:w-[600px] w-[310px]  h-[175px] rounded-md shadow-md p-3 bg-secondary border-border-gray"
+        />
+        <div className="pt-5 flex gap-7  justify-end">
+          <Button variant="outline" size="lg">
+            삭제하기
+          </Button>
+          <Button size="lg">등록하기</Button>
+        </div>
+      </div>
+    </div>
+  );
+}
