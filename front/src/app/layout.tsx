73a833f6d0f1f61cb7db25ba4c0c03d64c7b0d89
유현지 <bwj9808@gmail.com>
2025-09-15 16:49:54 +0900

    feat EXPBar 추가 및 폰트 설정 추가

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "../styles/globals.css";
 import Nav from "@/components/common/Nav";
 
 export const metadata: Metadata = {
--- front/src/app/main/page.tsx
+++ front/src/app/main/page.tsx
@@ -1,7 +1,12 @@
+import { ExpBar } from "@/components/main/ExpBar";
 import React from "react";
 
 const page = () => {
-  return <div className="w-full h-screen bg-gradient-main">메인화면</div>;
+  return (
+    <div className="w-full h-screen bg-gradient-main">
+      <ExpBar />
+    </div>
+  );
 };
 
 export default page;
