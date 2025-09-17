b3e372c3854cb6a720b502a73551436c6b327ffc
leejicircle <leeji.circle@gmail.com>
2025-09-17 23:07:13 +0900

    feat: 질문카드 분기처리

---
+++ front/src/components/question/Detail.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { useTodayQuestion } from '@/hooks/useQuestions';
+import React from 'react';
+import AnswerForm from './AnswerForm';
+import AnswerView from './AnswerView';
+
+export default function Detail() {
+  const { data, isLoading, isError } = useTodayQuestion();
+
+  if (isLoading) return <div>로딩중.</div>;
+  if (isError || !data) return <div>에러</div>;
+
+  const { status, question, myAnswer } = data;
+
+  return (
+    <>
+      {status === 'PENDING' && myAnswer === null && (
+        <AnswerForm mode="create" questionText={question.text} />
+      )}
+      {status === 'PENDING' && myAnswer !== null && (
+        <AnswerForm mode="edit" questionText={question.text} />
+      )}
+      {status === 'COMPLETED' && <AnswerView />}
+    </>
+  );
+}
