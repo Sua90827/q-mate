50ee96520efedd70c5ac363e23496b4b6ec23d4e
kyeonghooon <gibbon0510@gmail.com>
2025-09-22 01:05:04 +0900

    feat: 답변 제출 API 구현

---
+++ back/src/main/java/com/qmate/api/questioninstance/AnswerController.java
@@ -0,0 +1,37 @@
+package com.qmate.api.questioninstance;
+
+import com.qmate.domain.questioninstance.model.request.AnswerContentRequest;
+import com.qmate.domain.questioninstance.model.response.AnswerCreateResponse;
+import com.qmate.domain.questioninstance.service.AnswerService;
+import jakarta.validation.Valid;
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class AnswerController {
+
+  private final AnswerService answerService;
+
+  @PostMapping(value = "/question-instances/{questionInstanceId}/answers")
+  public ResponseEntity<AnswerCreateResponse> create(
+      @PathVariable Long questionInstanceId,
+      // TODO: @AuthenticationPrincipal CustomUserDetails principal
+      @Valid @RequestBody AnswerContentRequest request
+  ) {
+    Long userId = 1L; // TODO: principal.getUserId();
+
+    AnswerCreateResponse response = answerService.create(questionInstanceId, userId, request);
+
+    // Location: 생성으로 인해 최신 상태가 된 QI 상세 리소스
+    URI location = URI.create("/api/question-instances/" + questionInstanceId);
+    return ResponseEntity.created(location).body(response);
+  }
+}
--- back/src/main/java/com/qmate/common/constants/question/QuestionInstanceConstants.java
+++
@@ -1,5 +0,0 @@
-package com.qmate.common.constants.question;
-
-public class QuestionInstanceConstants {
-
-}
