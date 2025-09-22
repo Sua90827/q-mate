5e15fa6b3ebe48c9b8ca7fe00df01cde45b912ff
leejicircle <leeji.circle@gmail.com>
2025-09-22 16:36:49 +0900

    chore:mock script추가

--- front/src/api/invite.ts
+++ front/src/api/invite.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { InvitedCheck } from '@/types/InviteType';
 
 export const fetchInvitedCheck = async (): Promise<InvitedCheck> => {
-  const res = await axios.get<InvitedCheck>('http://localhost:3001/invited');
+  const res = await axios.get<InvitedCheck>('http://localhost:3004/invited');
   return res.data;
 };
