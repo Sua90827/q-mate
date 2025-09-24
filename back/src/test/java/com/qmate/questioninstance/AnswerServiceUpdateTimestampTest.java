044e1b06fa44e5ed3a7c1b8b967db3502e65341c
changwun <feelsogood7028@naver.com>
2025-09-24 17:04:41 +0900

    test: AnswerServiceUpdateTimestampTest 클래스 Disabled 어노테이션 삭제

--- back/src/test/java/com/qmate/questioninstance/AnswerServiceUpdateTimestampTest.java
+++ back/src/test/java/com/qmate/questioninstance/AnswerServiceUpdateTimestampTest.java
@@ -27,7 +27,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-@Disabled
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
