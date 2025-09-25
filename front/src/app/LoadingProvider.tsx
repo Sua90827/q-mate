078adb8d23e2893b60c0b5c88165450bdb1a1b2f
yuhyeon9808 <bwj9808@gmail.com>
2025-09-25 19:00:29 +0900

    chore 홈 화면에서만 보이게 수정

--- front/src/app/LoadingProvider.tsx
+++ front/src/app/LoadingProvider.tsx
@@ -2,21 +2,23 @@
 import React, { useEffect, useState } from 'react';
 import MainLoading from '../components/common/MainLoading';
 import { AnimatePresence, motion } from 'motion/react';
+import { usePathname } from 'next/navigation';
 
 export default function LoadingProvider({ children }: { children: React.ReactNode }) {
   const [show, setShow] = useState(true);
+  const pathname = usePathname();
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShow(false);
-    }, 1000);
+    }, 1500);
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <>
       <AnimatePresence>
-        {show && (
+        {show && pathname === '/' && (
           <motion.div initial={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 1 }}>
             <MainLoading />
           </motion.div>
