42944caa273dfb9dba02fe9fb5105dfd0893ecc3
leejicircle <leeji.circle@gmail.com>
2025-09-26 19:00:38 +0900

    chore:로딩상태 수정

--- front/src/components/schedule/list/EventList.tsx
+++ front/src/components/schedule/list/EventList.tsx
@@ -17,39 +17,45 @@ const repeatTypeLabels: Record<Schedule['repeatType'], string> = {
 };
 
 function EventList({ date, items, isLoading, isError }: Props) {
-  if (isLoading) {
-    return <div>일정을 불러오는 중입니다...</div>;
-  }
+  // if (isLoading) {
+  //   return <div>일정을 불러오는 중입니다...</div>;
+  // }
 
   if (isError) {
     return <div>일정을 불러오는 중 오류가 발생했습니다.</div>;
   }
 
   return (
-    <div className="flex-1 border-t border-text-text-primary rounded-b-lg bg-secondary shadow-sm h-full px-4 overflow-hidden">
+    <div className="flex-1 border-t flex flex-col border-text-text-primary rounded-b-lg bg-secondary shadow-sm h-full px-4 overflow-hidden">
       <h2 className="font-extrabold text-16 mt-4">
         {date.getDate()}. {date.toLocaleString('ko-kR', { weekday: 'narrow' })}
       </h2>
       {/* 리스트 내부에 스크롤 추가 */}
-      <ul className="max-h-50 flex-1 overflow-y-auto">
-        {items.length === 0 && <li>표시할 일정이 없어요.</li>}
+      <ul className="max-h- flex-1 min-h-0 overflow-y-auto">
+        {isLoading ? (
+          <li>일정을 불러오는 중...</li>
+        ) : (
+          <>
+            {items.length === 0 && <li>표시할 일정이 없어요.</li>}
 
-        {items.map(({ eventId, title, isAnniversary, repeatType }) => (
-          <li key={eventId} className="flex items-center py-2 border-y">
-            <span
-              className={`inline-block w-1 h-12 mr-2 ${
-                isAnniversary ? 'bg-anniversary' : 'bg-calendar'
-              }`}
-            />
-            <div>
-              <div className="font-extrabold">{title}</div>
-              <div className="text-12 text-text-secondary">
-                {isAnniversary ? '기념일' : '일정'}
-                {repeatType !== 'none' && ` · ${repeatTypeLabels[repeatType]}`}
-              </div>
-            </div>
-          </li>
-        ))}
+            {items.map(({ eventId, title, isAnniversary, repeatType }) => (
+              <li key={eventId} className="flex items-center py-2 border-y">
+                <span
+                  className={`inline-block w-1 h-12 mr-2 ${
+                    isAnniversary ? 'bg-anniversary' : 'bg-calendar'
+                  }`}
+                />
+                <div>
+                  <div className="font-extrabold">{title}</div>
+                  <div className="text-12 text-text-secondary">
+                    {isAnniversary ? '기념일' : '일정'}
+                    {repeatType !== 'none' && ` · ${repeatTypeLabels[repeatType]}`}
+                  </div>
+                </div>
+              </li>
+            ))}
+          </>
+        )}
       </ul>
     </div>
   );
