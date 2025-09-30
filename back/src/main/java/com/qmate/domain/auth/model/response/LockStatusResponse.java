536cd5931deab382dc0c0edd98fa500e201b6b8b
changwun <feelsogood7028@naver.com>
2025-09-30 15:31:15 +0900

    feat: 초대 코드 잠금 상태 조회를 위한 Response DTO 추가

---
+++ back/src/main/java/com/qmate/domain/auth/model/response/LockStatusResponse.java
@@ -0,0 +1,16 @@
+package com.qmate.domain.auth.model.response;
+
+import lombok.Getter;
+
+@Getter
+public class LockStatusResponse {
+
+  private final boolean isLocked;
+  private final Long remainingSeconds;// 남은 잠금 시간(초 단위)
+
+  public LockStatusResponse(boolean isLocked, Long remainingSeconds) {
+    this.isLocked = isLocked;
+    this.remainingSeconds = remainingSeconds;
+  }
+
+}
