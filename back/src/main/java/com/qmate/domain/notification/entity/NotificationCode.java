bcc46359f875c18ce720f29c9562aa2538a39689
kyeonghooon <gibbon0510@gmail.com>
2025-10-03 20:54:04 +0900

    feat: notification 엔티티 및 기본 JpaRepository 추가

---
+++ back/src/main/java/com/qmate/domain/notification/entity/NotificationCode.java
@@ -0,0 +1,13 @@
+package com.qmate.domain.notification.entity;
+
+public enum NotificationCode {
+  // QUESTION (QI)
+  QI_TODAY_READY,
+  QI_REMINDER,
+  QI_COMPLETED,
+
+  // EVENT
+  EVENT_DUE_SOON
+
+  // MATCH
+}
