cf7335394a63a62867ad7bcdd7ad455eb4cc9adf
kyeonghooon <gibbon0510@gmail.com>
2025-10-05 23:21:06 +0900

    feat: Pet/PetExpLog 엔티티 추가, Repository 추가

---
+++ back/src/main/java/com/qmate/domain/pet/repository/PetRepository.java
@@ -0,0 +1,10 @@
+package com.qmate.domain.pet.repository;
+
+import com.qmate.domain.pet.entity.Pet;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+
+  Optional<Pet> findByMatch_Id(Long matchId);
+}
