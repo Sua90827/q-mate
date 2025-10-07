82daeea71ddd68c8ea3f27bc68353e0fac064414
yuhyeon9808 <bwj9808@gmail.com>
2025-10-07 17:35:47 +0900

    design 홈 화면 디자인 수정

--- front/src/components/home/Home.tsx
+++ front/src/components/home/Home.tsx
@@ -5,7 +5,7 @@ import { Button } from '../common/Button';
 
 export default function Home() {
   return (
-    <div className="w-full h-full  flex flex-col items-center justify-center">
+    <div className="w-full h-full  flex flex-col items-center justify-center pb-[70px]">
       <div className="absolute inset-0 pointer-events-none z-0">
         <picture>
           <source media="(max-width: 768px) " srcSet="/images/background_deco_M.png" />
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="flex flex-col gap-5 items-center justify-center">
         <Image src="/images/logo/day_logo.svg" alt="큐메이트" width={173} height={55} />
         <p className="font-Gumi text-24">함께 하루를 기록해봐요!</p>
-        <Button variant="invite" className="w-[300px] mt-10" asChild>
+        <Button variant="invite" className="w-[300px] mt-6" asChild>
           <Link href="/login">시작 하기</Link>
         </Button>
       </div>
