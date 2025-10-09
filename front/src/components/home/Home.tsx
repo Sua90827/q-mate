25ae04f016681e9f8f8ab6c5268ea328d2fb5017
yuhyeon9808 <bwj9808@gmail.com>
2025-10-09 13:10:09 +0900

    chore 시작화면 커서 포인터 추가

--- front/src/components/home/Home.tsx
+++ front/src/components/home/Home.tsx
@@ -51,7 +51,7 @@ export default function Home() {
       <div className="flex flex-col gap-5 items-center justify-center">
         <Image src="/images/logo/day_logo.svg" alt="큐메이트" width={173} height={55} />
         <p className="font-Gumi text-24">함께 하루를 기록해봐요!</p>
-        <Button variant="invite" className="w-[300px] mt-6" onClick={checkLogin}>
+        <Button variant="invite" className="w-[300px] mt-6 cursor-pointer" onClick={checkLogin}>
           시작 하기
         </Button>
       </div>
